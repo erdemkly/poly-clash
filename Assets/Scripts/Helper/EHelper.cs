305a10cb5999dc4dd5c9114a81530a54812c4e61
erdemkly <50420788+erdemkly@users.noreply.github.com>
2022-03-26 15:11:11 +0300

    add plugins

---
+++ Assets/Scripts/Helper/EHelper.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Helper
+{
+    public static class EHelper
+    {
+        public static Vector3 MyLookAt(this Vector3 euler, Vector3 targetPos, Vector3 currentPos)
+        {
+            var delta = targetPos - currentPos;
+            euler.y = Mathf.Atan2(delta.y, delta.x) * Mathf.Rad2Deg;
+            return euler;
+        }
+        public static Vector3 ClampVector3(this Vector3 position, Vector3 min, Vector3 max)
+        {
+            position.x = Mathf.Clamp(position.x, min.x, max.x);
+            position.y = Mathf.Clamp(position.y, min.y, max.y);
+            position.z = Mathf.Clamp(position.z, min.z, max.z);
+
+            return position;
+        } 
+        public static Vector2 ClampVector2(this Vector2 position, Vector2 min, Vector2 max)
+        {
+            position.x = Mathf.Clamp(position.x, min.x, max.x);
+            position.y = Mathf.Clamp(position.y, min.y, max.y);
+
+            return position;
+        }
+        public static float GetDirection(this Vector2 vector,Vector2 direction)
+        {
+            vector.x *= direction.x;
+            vector.y *= direction.y;
+            return vector.x+vector.y;
+        }
+
+        /// <summary>
+        /// return (v.x,v.y,0)
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public static Vector3 ToVector3Z0(this Vector2 v)
+        {
+            return new Vector3(v.x, v.y, 0);
+        }
+        
+        /// <summary>
+        /// return (v.x,0,v.y)
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public static Vector3 ToVector3Y0(this Vector2 v)
+        {
+            return new Vector3(v.x, 0, v.y);
+        }
+        
+        /// <summary>
+        /// return (0,v.x,v.y)
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public static Vector3 ToVector3X0(this Vector2 v)
+        {
+            return new Vector3(0, v.x, v.y);
+        }
+
+        /// <summary>
+        /// return ({x},v.y,v.z)
+        /// </summary>
+        /// <param name="v"></param>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public static Vector3 SetX(this Vector3 v,float x)
+        {
+            return new Vector3(x, v.y, v.z);
+        }
+        /// <summary>
+        /// return (v.x,{y},v.z)
+        /// </summary>
+        /// <param name="v"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public static Vector3 SetY(this Vector3 v,float y)
+        {
+            return new Vector3(v.x, y, v.z);
+        }
+        /// <summary>
+        /// return (v.x,v.y,{z})
+        /// </summary>
+        /// <param name="v"></param>
+        /// <param name="z"></param>
+        /// <returns></returns>
+        public static Vector3 SetZ(this Vector3 v,float z)
+        {
+            return new Vector3(v.x, v.y, z);
+        }
+        
+        public static Vector3 ToInt(this Vector3 v)
+        {
+            return new Vector3((int)v.x, (int)v.y, (int)v.z);
+        } 
+        public static Vector2 ToInt(this Vector2 v)
+        {
+            return new Vector2((int)v.x, (int)v.y);
+        }
+
+        public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layerMask)
+        {
+            var randDirection = Random.insideUnitSphere * dist;
+            randDirection += origin;
+            NavMesh.SamplePosition(randDirection, out var navHit, dist, layerMask);
+            return navHit.position;
+        }
+        public static int LayerMaskToLayer(this LayerMask layerMask)
+        {
+            var layerNumber = 0;
+            var layer = layerMask.value;
+            while (layer > 0)
+            {
+                layer = layer >> 1;
+                layerNumber++;
+            }
+            return layerNumber - 1;
+        }
+        public static float Clamp0360(this float eulerAngles)
+        {
+            var result = eulerAngles - Mathf.CeilToInt(eulerAngles / 360f) * 360f;
+            
+            if (result < 0)
+            {
+                result += 180f;
+            }
+            return result;
+        }
+
+
+    }
+    public static class RandomItemGeneric<T>
+    {
+        public static T GetRandom(params T[] array)
+        {
+            var rand = Random.Range(0, array.Length);
+            return array[rand];
+        }
+
+        public static T[] GetRandomMultiple(int count, bool canRepeat = true, params T[] array)
+        {
+            var result = new List<T>();
+            count = Mathf.Clamp(count, 0, array.Length);
+            for (int i = 0; i < count; i++)
+            {
+                T randItem;
+                do
+                {
+                    randItem = GetRandom(array);
+                }
+                while (!canRepeat && result.Contains(randItem));
+
+                result.Add(randItem);
+            }
+            return result.ToArray();
+        }
+        public static T[] GetMixedArray(params T[] array)
+        {
+            var result = array;
+            for (int i = 0; i < array.Length; i++)
+            {
+                var rand = Random.Range(0, result.Length);
+                var tmp = result[i];
+                result[i] = result[rand];
+                result[rand] = tmp;
+            }
+            return result;
+        }
+    }
+}
---
+++ Assets/Scripts/Helper/TransformFollower.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NaughtyAttributes;
+using UnityEditor;
+using UnityEngine;
+
+[Serializable]
+public struct MyAxis
+{
+    public bool x, y, z;
+}
+[Serializable]
+public struct MyClamp
+{
+    public bool x, y, z;
+    [EnableIf("x")]public float minX, maxX;
+    [EnableIf("y")]public float minY, maxY;
+    [EnableIf("z")]public float minZ, maxZ;
+}
+public class TransformFollower : MonoBehaviour
+{
+    [SerializeField,Required()] public Transform target;
+
+    public MyAxis positionAxis,scaleAxis,eulerAxis,quaternionAxis;
+    public MyClamp clampedAxis;
+    [SerializeField] public Vector3 posOffset;
+
+    private Camera _cam;
+    public enum FollowType
+    {
+        WorldToWorld,
+        ScreenToWorld,
+        WorldToScreen,
+        ScreenToScreen
+    }
+    public FollowType followType;
+
+    public float smoothness = 0.125f;
+
+    private void OnEnable()
+    {
+        if (_cam == null) _cam = Camera.main;
+    }
+
+    [Button("Initialize")]
+    private void Start()
+    {
+        Follow(1);
+    }
+    private void LateUpdate()
+    {
+        Follow(smoothness);
+    }
+    private Vector3 InitFollowType()
+    {
+        Vector3 targetPos;
+        switch (followType)
+        {
+            case FollowType.WorldToWorld:
+                targetPos = target.position + posOffset;
+                break;
+            case FollowType.ScreenToWorld:
+                targetPos = _cam.WorldToScreenPoint(target.position+posOffset);
+                targetPos.z = 0;
+                break;
+            case FollowType.WorldToScreen:
+                targetPos = _cam.ScreenToWorldPoint(target.position+posOffset);
+                break;
+            case FollowType.ScreenToScreen:
+                targetPos = target.position+posOffset;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+        return targetPos;
+    }
+    private void SetPosition(float s)
+    {
+        if (!positionAxis.x && !positionAxis.y && !positionAxis.z) return;
+        var pos = transform.position;
+        var targetPos = InitFollowType();
+        if (positionAxis.x)
+        {
+            if (clampedAxis.x) targetPos.x = Mathf.Clamp(targetPos.x, clampedAxis.minX, clampedAxis.maxX);
+            pos.x = targetPos.x;
+        }
+        if (positionAxis.y)
+        {
+            if (clampedAxis.y) targetPos.y = Mathf.Clamp(targetPos.y, clampedAxis.minY, clampedAxis.maxY);
+            pos.y = targetPos.y;
+        }
+        if (positionAxis.z)
+        {
+            if (clampedAxis.z) targetPos.z = Mathf.Clamp(targetPos.z, clampedAxis.minZ, clampedAxis.maxZ);
+            pos.z = targetPos.z;
+        }
+        transform.position = Vector3.Lerp(transform.position, pos, s);
+    }
+    private void SetScale(float s)
+    {
+        if (!scaleAxis.x && !scaleAxis.y && !scaleAxis.z) return;
+        
+        var localScale = transform.localScale;
+        
+        if (scaleAxis.x) localScale.x = target.localScale.x;
+        if (scaleAxis.y) localScale.y = target.localScale.y;
+        if (scaleAxis.z) localScale.z = target.localScale.z;
+        
+        transform.localScale = Vector3.Lerp(transform.localScale, localScale, s);
+    }
+    private void SetEuler()
+    {
+        if (!eulerAxis.x && !eulerAxis.y && !eulerAxis.z) return;
+        
+        var euler = transform.eulerAngles;
+        
+        if (eulerAxis.x) euler.x = target.eulerAngles.x;
+        if (eulerAxis.y) euler.y = target.eulerAngles.y;
+        if (eulerAxis.z) euler.z = target.eulerAngles.z;
+
+        transform.eulerAngles = euler;
+    }
+    private void SetQuaternion(float s)
+    {
+        if (!quaternionAxis.x && !quaternionAxis.y && !quaternionAxis.z) return;
+        
+        var quaternion = transform.rotation;
+        
+        if (quaternionAxis.x) quaternion.x = target.rotation.x;
+        if (quaternionAxis.y) quaternion.y = target.rotation.y;
+        if (quaternionAxis.z) quaternion.z = target.rotation.z;
+
+        transform.rotation = Quaternion.Lerp(transform.rotation,quaternion,s);
+    }
+    private void Follow(float s)
+    {
+        if (target == null) return;
+        SetPosition(s);
+        SetScale(s);
+        SetEuler();
+        SetQuaternion(s);
+    }
+
+
+}
